4baea77a6d268a0f7f9fa51940086108605949f5
Mustafizur <sayhemrahman@gmail.com>
2021-10-07 11:15:51 +0600

    added services and router

--- src/App.js
+++ src/App.js
@@ -1,11 +1,44 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
 import './App.css';
+import About from './Components/About/About';
+import Home from './Components/Home/Home';
+import Services from './Components/Services/Services';
 
 function App() {
   return (
     <div className="App">
 
-      <h1> Education Course</h1>
+      <Router>
+        <Switch>
+
+          <Route exact path="/">
+            <Home></Home>
+          </Route>
+          <Route path="/home">
+            <Home></Home>
+          </Route>
+          <Route path="/about">
+            <About></About>
+          </Route>
+          <Route path="/services">
+            <Services></Services>
+          </Route>
+
+
+
+
+
+        </Switch>
+
+
+
+
+      </Router>
 
     </div>
   );
---
+++ src/Components/Services/Services.js
@@ -0,0 +1,17 @@
+import React, { useEffect, useState } from 'react';
+
+const Services = () => {
+
+    const [allcourse, setAllcourse] = useState([]);
+    useEffect(() => {
+
+    }, [])
+
+    return (
+        <div>
+            <h1>This is services</h1>
+        </div>
+    );
+};
+
+export default Services;
