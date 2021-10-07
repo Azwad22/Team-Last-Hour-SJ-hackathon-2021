a92b76a8036cfeec6cf1fb8bcf3f8a99d96a2e51
Mustafizur <sayhemrahman@gmail.com>
2021-10-07 13:18:45 +0600

    adding instructor page

--- src/Components/Services/Services.js
+++ src/Components/Services/Services.js
@@ -1,8 +1,6 @@
 import React, { useEffect, useState } from 'react';
-import { addToDb } from '../../LocalStorage/localStorage';
 import Allcourses from '../Allcourses/Allcourses';
 
-import useCart from '../../CustomHooks/useCart';
 
 const Services = () => {
 
@@ -16,20 +14,7 @@ const Services = () => {
 
 
     const handleAddToCart = (product) => {
-        const exists = cart.find(pd => pd.key === product.key);
-        let newCart = [];
-        if (exists) {
-            const rest = cart.filter(pd => pd.key !== product.key);
-            exists.quantity = exists.quantity + 1;
-            newCart = [...rest, product];
-        }
-        else {
-            product.quantity = 1;
-            newCart = [...cart, product];
-        }
-        setCart(newCart);
-        // save to local storage (for now)
-        addToDb(product.key);
+
 
     }
 
