13cf84be75153f357185bb8ad76459b5706842f5
Azwad22 <mdazwad22@gmail.com>
2021-10-07 15:55:42 +0600

    finalhome

--- src/App.js
+++ src/App.js
@@ -1,26 +1,12 @@
-<<<<<<< HEAD
-import 'bootstrap/dist/css/bootstrap.min.css';
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route
-} from "react-router-dom";
-import './App.css';
-import About from './Components/About/About';
-import BuyCourse from './Components/BuyCourse/BuyCourse';
-import Home from './Components/Home/Home';
-import Instructor from './Components/Instructor/Instructor';
-import Services from './Components/Services/Services';
-
-=======
 import "bootstrap/dist/css/bootstrap.min.css";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import "./App.css";
 import About from "./Components/About/About";
+import BuyCourse from "./Components/BuyCourse/BuyCourse";
 import Home from "./Components/Home/Home";
+import Instructor from "./Components/Instructor/Instructor";
 import Services from "./Components/Services/Services";
-import BuyCourse from "./Components/BuyCourse/BuyCourse.js";
->>>>>>> 3c4273f71a89531c698f7d503388ff332d40d831
+
 function App() {
   return (
     <div className="App">
@@ -41,18 +27,10 @@ function App() {
           <Route path="/buycourse">
             <BuyCourse></BuyCourse>
           </Route>
-<<<<<<< HEAD
 
           <Route path="/instructor">
             <Instructor></Instructor>
           </Route>
-
-
-
-
-
-=======
->>>>>>> 3c4273f71a89531c698f7d503388ff332d40d831
         </Switch>
       </Router>
     </div>
